d015b1aae60e2b5c7642e383ce334a223c6f3b85
gkovan <gkovan@hotmail.com>
2021-04-09 09:18:25 -0400

    moved code to the template

---
+++ src/main/java/com/ibm/mqclient/ApplicationExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ibm.mqclient;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.ibm.mqclient.exceptions.AppException;
+import com.ibm.mqclient.model.ErrorResponse;
+
+@Provider
+public class ApplicationExceptionHandler implements ExceptionMapper<Throwable>  {
+		
+    public Response toResponse(AppException e) 
+    {
+    	ErrorResponse error = new ErrorResponse(e.getErrorCode(), e.getLocalizedMessage());
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(error).build();  
+    }
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		// TODO Auto-generated method stub
+		AppException e = (AppException) exception;
+    	ErrorResponse error = new ErrorResponse(e.getErrorCode(), e.getLocalizedMessage());
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(error).build();  
+	}
+
+}
